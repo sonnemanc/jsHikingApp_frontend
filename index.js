d47a8771d54ccde0b6bf869f887bbdc797579341
Corban <sonnemanc@gmail.com>
2022-02-26 16:48:52 -0800

    listener event working for map buttons

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("in index.js");
+
+window.addEventListener('DOMContentLoaded', function() {
+const dots = document.querySelectorAll('.dot');
+for(let dot1 of dots) {
+    dot1.addEventListener('click', function() {
+        console.log('I was clicked!');
+      });
+}
+
+})
+
+
+console.log('You are in the console!')
