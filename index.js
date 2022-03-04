dd5e755df932e7d935595e51f6edcbeaaca6326a
Corban <sonnemanc@gmail.com>
2022-03-03 16:28:57 -0800

    linked dots with their hikes

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 
-window.addEventListener('DOMContentLoaded', function() {
-const dots = document.querySelectorAll('.dot');
-for(let dot1 of dots) {
-    dot1.addEventListener('click', function() {
-        console.log('I was clicked!');
-      });
+
+// Url for fetch function
+let hikeUrl = "http://localhost:3000/api/v1/hikes"
+
+//function to fetch hikes array
+let fetchHikes = () => fetch(hikeUrl).then(response => response.json())
+
+let hikes = []
+
+function getHikes() {
+  fetch(hikeUrl)
+  .then(response => response.json())
+  .then(hike => {
+    hike.data.forEach(hike => {
+      hikes.push(hike)
+    })
+  })
 }
 
+
+function addListener() {
+  const dots = document.querySelectorAll('.dot');
+  const dotsArr = Array.from(dots);
+  for (let i = 0; i < dotsArr.length; i++) {
+    const element = dotsArr[i]
+    element.addEventListener('click', function() {
+      console.log(hikes[i].attributes.name)
+    })
+  }
+}
+
+//dot1.addEventListener('click', function() {}
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  getHikes()
+  addListener()
 })
 
-
-console.log('You are in the console!')
